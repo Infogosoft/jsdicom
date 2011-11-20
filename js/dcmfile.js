2d9c385de1af0a513aafa3c82e9fedf1d7dbca79
diggidanne <daniel@d-dog.se>
2011-11-20 14:52:09 +0100

    Forgot file..

--- js/dcmfile.js
+++ js/dcmfile.js
@@ -8,6 +8,14 @@ function DataElement(tag, vr, vl, data) {
 function DcmFile() {
     // File Meta Information
     this.meta_elements = [];
+    this.get_meta_element = function(tag) {
+        for(var i=0;i<this.meta_elements.length;++i) {
+            if(this.meta_elements[i].tag == tag)
+                return this.meta_elements[i];
+        }
+        return 0;
+    }
+
     this.data_elements = [];
     this.get_element = function(tag) {
         for(var i=0;i<this.data_elements.length;++i) {
@@ -16,4 +24,11 @@ function DcmFile() {
         }
         return 0;
     }
+    this.buffer;
+    this.rescaleSlope;
+    this.rescaleIntercept;
+    this.pixel_data;
+    this.width;
+    this.height;
+    //this.pixel_data;
 }
