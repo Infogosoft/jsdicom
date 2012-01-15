d9467de4ba4dcbda36b18fb08a498f6eb10782c5
Rickard Holmberg <rickard@holmberg.info>
2012-01-15 21:51:43 +0100

    Preliminary support for implicit VR

--- js/transfersyntax.js
+++ js/transfersyntax.js
@@ -37,7 +37,7 @@ function read_tag_LE(buffer, offset) {
     return tag;
 }
 
-function element_reader(tag_reader, number_reader) {
+function element_reader(tag_reader, number_reader, implicit) {
     this._read_tag = tag_reader;
     this._read_number = number_reader;
 
@@ -47,33 +47,64 @@ function element_reader(tag_reader, number_reader) {
         offset += 4;
 
         var vl;
-        var vr = read_vr(buffer, offset);
-        if(vr == "OB" || vr == "OF" || vr == "SQ" || vr == "OW" || vr == "UN") { 
-            offset += 4;
-            vl = this._read_number(buffer, offset, 4);
-            offset += 4;
-        } else {
-            offset += 2;
-            vl = this._read_number(buffer, offset, 2);
-            offset += 2;
-        }
+        var vr;
 
+	if(implicit) {
+	    vr = "UN";
+	    if(tag in tags_vr) {
+		vr = tags_vr[tag];
+	    }
+	    
+	    vl = this._read_number(buffer, offset, 4);
+	    offset += 4;
+	} else {
+	    vr = read_vr(buffer, offset);
+	    if(vr == "OB" || vr == "OF" || vr == "SQ" || vr == "OW" || vr == "UN") { 
+		offset += 4;
+		vl = this._read_number(buffer, offset, 4);
+		offset += 4;
+            } else {
+		offset += 2;
+		vl = this._read_number(buffer, offset, 2);
+		offset += 2;
+            }
+	}
+	
         element.tag = tag;
         element.vr = vr;
         element.vl = vl;
         element.data = buffer.subarray(offset, offset+vl);
-
+	
         offset += vl;
         return offset;
     }
 }
 
+tags_vr = {
+    0x7fe00010: "OW", // PixelData
+    0x00100010: "PN", // Patients Name
+    0x00280010: "US", // Rows
+    0x00280011: "US", // Columns
+    0x00281050: "DS", // WindowCenter
+    0x00281051: "DS", // WindowWidth
+    0x00281052: "DS", // RescaleIntercept
+    0x00281053: "DS", // RescaleSlope
+}
+
 tag_readers = {
     "1.2.840.10008.1.2.1": read_tag_LE,
-    "1.2.840.10008.1.2.2": read_tag_BE
+    "1.2.840.10008.1.2.2": read_tag_BE,
+    "1.2.840.10008.1.2": read_tag_LE,
+}
+
+is_implicit = {
+    "1.2.840.10008.1.2.1": false,
+    "1.2.840.10008.1.2.2": false,
+    "1.2.840.10008.1.2": true,
 }
 
 number_readers = {
+    "1.2.840.10008.1.2": read_number_LE,
     "1.2.840.10008.1.2.1": read_number_LE,
     "1.2.840.10008.1.2.2": read_number_BE
 }
@@ -82,7 +113,8 @@ number_readers = {
 function get_element_reader(transfersyntaxUID) {
     if(transfersyntaxUID in tag_readers && transfersyntaxUID in number_readers) {
         return new element_reader(tag_readers[transfersyntaxUID],
-                                  number_readers[transfersyntaxUID])
+                                  number_readers[transfersyntaxUID],
+                                  is_implicit[transfersyntaxUID])
     }
     return;
 }
